1f29b3d0aced598639b957c6b7d59007cb32dd1b
lidya <lidya. abraham@student.moringaschool.com>
2022-08-28 18:24:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(games) {
+    let win = games.find(game => game.result === 'W');
+    if(typeof(win) === 'undefined') {
+        return undefined;
+    } else {
+        return win.year;
+    }
+}
